89dcdc06c8c31f7e20abd9734fd2976cf17c4ca9
Ramon Rodriguez Quiñonez <lmartinez.bno@gmail.com>
2018-04-24 12:53:13 -0600

    Primer JQuery

---
+++ js/primerjquery.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+	//JSON = JavaScript Object Notation
+
+	function nuevo () {
+		$.ajax({
+			url: 'https://randomuser.me/api/',
+		  	dataType: 'json',
+		  	success: function(data) {
+		  		console.log(data);
+		  		$('#nombre').html(data.results[0].name.first+" "+data.results[0].name.last);
+		  		$('#foto').attr('src', data.results[0].picture.large);
+		  	}
+		});
+	}
+
+	$('#btnNuevo').on('click', function () {
+		nuevo();
+	});
+});
