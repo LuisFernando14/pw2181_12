4fa3f510fbe65373ce9db007f201144188914552
Ramon Rodriguez Quiñonez <lmartinez.bno@gmail.com>
2018-04-25 13:01:56 -0600

    Marvel - Pendiente

---
+++ marvel/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+		$('#btnBuscar').on('click', function () {
+			var personaje = $("#txtPersonaje").val();
+			var url = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith=";
+			var cantidadComics = 0;
+			var comics = "";
+			url = url + personaje;
+			console.log(personaje);
+			$.ajax({
+				dataType : "json",
+				url : url,
+				success: function (response) {
+					if(response.code = 200) {
+						console.log(response);
+						$("#foto").attr('src', response.data.results[0].thumbnail.path+"."+response.data.results[0].thumbnail.extension);
+					}
+				}
+			});
+		});
+});
